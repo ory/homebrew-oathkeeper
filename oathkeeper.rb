0b669eb004e2ed7bb913274027cdc52f4afb7b2d
ory <hi@ory.sh>
2019-05-19 12:49:54 +0200

    Brew formula update for oathkeeper version v0.16.0-beta.3

--- oathkeeper.rb
+++ oathkeeper.rb
@@ -2,9 +2,9 @@
 class Oathkeeper < Formula
   desc ""
   homepage "https://www.ory.sh"
-  url "https://github.com/ory/oathkeeper/releases/download/v0.15.2/oathkeeper_0.15.2_macOS_64-bit.tar.gz"
-  version "0.15.2"
-  sha256 "25e28b26cc4c34edc12219c8422c0cdb1a1d9af358c857d77ee2fbbf3e15ece6"
+  url "https://github.com/ory/oathkeeper/releases/download/v0.16.0-beta.3/oathkeeper_0.16.0-beta.3_macOS_64-bit.tar.gz"
+  version "0.16.0-beta.3"
+  sha256 "77c50e1a5bd6b0fbef3aacacb56fe12260cd0f01d050007816001fe3681d16cd"
 
   def install
     bin.install "oathkeeper"
